f8752332905c6d19e06dbd88e431f02605b98e6d
dongzhiqiang <1217327656@qq.com>
2025-08-11 15:49:25 +0800

    feat: update deploy yaml

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig4CustomTheme({
       description: '前端编码规范工程化',
     },
   },
-  base: '/dongzq0624/',
+  base: '/fe-spec/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/index.md' },
